3d506bb2c8e08a7e1c95e2f59285b87e1e42f805
skiwi <frank_skiwi@hotmail.com>
2016-09-29 17:26:16 +0200

    Solved Euler Problem 8.

---
+++ src/project_euler/problems/problem8.clj
@@ -0,0 +1,15 @@
+(ns project-euler.problems.problem8
+  (:require [clojure.java.io :as io]))
+
+(defn read-1000-digit-number []
+  (with-open [rdr (io/reader (io/resource "problem8.txt"))]
+    (apply str (line-seq rdr))))
+
+(defn largest-product-in-a-series [len]
+  {:pre [(pos? len)]}
+  (->> (partition len 1 (read-1000-digit-number))
+       (map (partial map (comp read-string str)))
+       (map (partial reduce *))
+       (apply max)))
+
+(println (largest-product-in-a-series 13))
